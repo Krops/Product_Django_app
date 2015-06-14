77d076054c626be349cf58c1fa46fbfe2e7a7847
Krop <andrekropes@yandex.ru>
2015-06-14 21:40:27 +0300

    comments added

---
+++ mysite/product/urls.py
@@ -0,0 +1,25 @@
+'''from django.conf.urls import url
+
+from . import views
+handler404 = 'mysite.views.my_custom_page_not_found_view'
+urlpatterns = [
+    # ex: /polls/
+    url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    #url(r'^(?P<poll_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^specifics/(?P<poll_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<poll_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<poll_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]'''
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='list'),
+    url(r'^(?P<product_id>[0-9]+)/$', views.detail, name='detail'),
+    #url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<product_id>[0-9]+)/add_comment/$', views.add_comment, name='add_comment'),
+]
